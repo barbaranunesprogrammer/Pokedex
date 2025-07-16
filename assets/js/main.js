3be604ce6c09a6ea4587dd329caf2dba73b740ba
Barbara Nunes <barbaranunesprogrammer@outlook.com>
2025-07-15 23:04:34 -0300

    primeiro commit

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+const loadMoreButton = document.getElementById("loadMoreButton");
+const limit = 10;
+let offset = 0;
+
+const pokemonList = document.getElementById("pokemonList");
+
+function pokemonToLi(pokemon) {
+    return `
+        <li class="pokemon ${pokemon.type}" onclick="window.location.href='pokedex.html?pokemon=${pokemon.name}'">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+                <img src="${pokemon.photo}" alt="${pokemon.name}">
+            </div>
+        </li>`;
+}
+
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map(pokemonToLi).join("");
+        pokemonList.innerHTML += newHtml;
+    });
+}
+
+// Carrega os primeiros 5 Pokémon
+loadPokemonItens(offset, limit);
+
+// Ao clicar no botão, carrega mais 5
+loadMoreButton.addEventListener("click", () => {
+    offset += limit;
+    loadPokemonItens(offset, limit);
+});
---
+++ assets/js/poke.js
@@ -0,0 +1,37 @@
+function getPokemonNameFromURL() {
+    const params = new URLSearchParams(window.location.search);
+    return params.get("pokemon");
+}
+
+function showPokemonDetail(pokemon) {
+    const div = document.getElementById("pokemonDetail");
+    div.innerHTML = `
+        <h2 class = "name-poke">${pokemon.name}</h2>
+        <div class="details-poke">
+<img class = "img-poke" src="${pokemon.sprites.other['official-artwork'].front_default}" alt="${pokemon.name}">
+<p><strong>Type:</strong> ${pokemon.types.map(t => t.type.name).join(', ')}</p>
+<p><strong>Height:</strong> ${pokemon.height / 10} m</p>
+<p><strong>Weight:</strong> ${pokemon.weight / 10} kg</p>
+<p><strong>Abilities:</strong> ${pokemon.abilities.map(a => a.ability.name).join(', ')}</p>
+<p><strong>Moves:</strong> ${pokemon.moves.map(m => m.move.name).join(', ')}</p>
+<p><strong>Base Experience:</strong> ${pokemon.base_experience}</p>
+   </div>
+    
+    `;
+}
+
+const pokemonName = getPokemonNameFromURL();
+
+fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+    .then(response => response.json())
+    .then(showPokemonDetail)
+    .catch(err => console.error("Erro ao carregar detalhes", err));
+
+    const button = document.getElementById("voltar");
+
+    function botaoVoltar() {
+       //quando eu clicar vai voltar para o index.html
+       window.location.href = "index.html";
+    }
+
+    button.addEventListener("click", botaoVoltar);
